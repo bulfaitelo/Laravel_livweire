8183a3e99aa3969b08a622eb3a351c8398adfc62
Bulfaitelo <bulfaitelo@gmail.com>
2023-08-19 11:45:33 -0300

    Aula 10 (jetsteam)

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('tweets', ShowTweets::class);
+Route::get('tweets', ShowTweets::class)->middleware('auth');
 
 Route::get('/', function () {
-    return view('layouts.app');
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
