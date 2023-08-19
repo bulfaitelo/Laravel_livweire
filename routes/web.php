d1dd70ba18e267d5894ac9bce5f7dff8efc5d239
Bulfaitelo <bulfaitelo@gmail.com>
2023-08-19 09:41:49 -0300

    Aula 05

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\ShowTweets;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('tweets', ShowTweets::class);
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
